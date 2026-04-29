346866a94e699d2163e78cd672ff5d1578f15ad6
Thomas Lichtenecker <t.lichtenecker@gmail.com>
2026-04-29 11:01:09 +0200

    chore: format extension path validation tests

--- test/unit/superagents-config.test.ts
+++ test/unit/superagents-config.test.ts
@@ -12,7 +12,7 @@ import assert from "node:assert/strict";
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
-import { describe, it, before, after } from "node:test";
+import { describe, it } from "node:test";
 
 import {
 	findMissingSubagentExtensionPath,
